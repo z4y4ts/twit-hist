c7b10db6494ba642d0a954feb70deb57d5af9e76
Alexander Zayats <alexander.zayats@gmail.com>
2012-04-28 23:59:31 +0300

    crawler updated.

--- crawler.py
+++ crawler.py
@@ -1,44 +1,74 @@
 #!/usr/bin/env python
 
-from urllib import urlencode, urlopen
 import json
 import urllib
-import pprint
+from datetime import datetime
 
 from pymongo import Connection
+import protection
+
+import data
+
+
+def to_datetime(string):
+    return protection.str2date(string, '%a, %d %b %Y %H:%M:%S +0000')
 
 
 def fetch_tweets(htags):
     base_url = 'http://search.twitter.com/search.json?q='
-    # url = 'http://search.twitter.com/search.json?q=%40douhack'
+    res = {}
     for htag in htags:
         url = base_url + urllib.quote(htag)
-        print 'fetching ', url
-        d = json.loads(urlopen(url).read())
+        print('fetching ', url)
+        d = json.loads(urllib.urlopen(url).read())
         raw_tweets = d['results']
         next_page = d.get('next_page', None)
         while next_page:
             url = urllib.basejoin(base_url, next_page)
-            print 'fetching ', url
-            d = json.loads(urlopen(url).read())
+            print('fetching ', url)
+            d = json.loads(urllib.urlopen(url).read())
             raw_tweets += d['results']
             next_page = d.get('next_page', None)
-        return raw_tweets
+            # next_page = None
+        res[htag] = raw_tweets
+    return res
 
 
 def save_tweet(htag, tweet):
     conn = Connection()
     db = conn.twit
-    tweet_data = {'hashes': [htag], 'data': json.dumps(tweet)}
-    db.tweet.insert(tweet_data)
+    try:
+        tweet_data = {'htags': [htag],
+                  'id': tweet['id'],
+                  'datetime': to_datetime(tweet['created_at']),
+                  'text': tweet['text']}
+    except:
+        pass
+    try:
+        if not db.tweet.find_one({'id': tweet['id']}):
+            db.tweet.insert(tweet_data)
+    except:
+        pass
+
+
+def find_tweets(htag, date_from, date_to):
+    conn = Connection()
+    db = conn.twit
+    dt_from = {'datetime': {'$gte': date_from}}
+    dt_to = {'datetime': {'$lte': date_to}}
+    query = {'htags': htag,
+             '$and': [dt_from, dt_to]}
+    tweets = db.tweet.find(query)
+    return (_ for _ in tweets)
 
 
 def main():
-    tags = ['#douhack', '#uwcua']
-    tags = ['#douhack']
+    tags = ['#garage48']
+    # tags = ['#douhack']
     tweets = fetch_tweets(tags)
-    # for tweet in tweets:
-    #     save_tweet(tweet)
+    for tweet in tweets:
+        # print tweet
+        save_tweet('#douhack', tweet)
 
 
 if __name__ == '__main__':
