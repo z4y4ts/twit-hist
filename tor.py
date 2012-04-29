1d06e41ddc21cfabbec428133135c5ee9542ebc3
Svyatoslav Sydorenko aka webKnjaZ <wk.git.douhack@sydorenko.org.ua>
2012-04-29 06:08:16 +0300

    Watching after template index.html and all the static files folder

--- tor.py
+++ tor.py
@@ -23,12 +23,12 @@ class HomeHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html', **{'htag': '#douhack'})
 
-
+static_path = 'static/htdocs/'
 
 settings = {
     'template_path': os.path.join(os.path.dirname(__file__), "templates"),
     'xsrf_cookies': True,
-    'static_path':os.path.join(os.path.dirname(__file__), "static/htdocs"),
+    'static_path':os.path.join(os.path.dirname(__file__), static_path),
 }
 
 import tornado.options
@@ -42,5 +42,7 @@ if __name__ == "__main__":
     application.listen(8888)
     ioloop = tornado.ioloop.IOLoop.instance()
     from tornado import autoreload
+    tornado.autoreload.watch(static_path)
+    tornado.autoreload.watch('templates/index.html')
     autoreload.start(ioloop)
     ioloop.start()
